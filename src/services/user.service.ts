9ea8fcc9b35aff6951a8f0a78ac775f38178da6d
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 09:00:11 +0530

    feat: integrate src/services/user.service.ts

---
+++ src/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): any {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
